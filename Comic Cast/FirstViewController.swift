96f297e63477141bbd9a75313e50da2d8ca7aa83
Bjarne Tvedten <bjarnet3@hotmail.com>
2019-01-20 14:22:33 +0100

    Add iOS Project

---
+++ Comic Cast/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Comic Cast
+//
+//  Created by Bjarne Tvedten on 20/01/2019.
+//  Copyright © 2019 Digital Mood. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Comic Cast/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Comic Cast
+//
+//  Created by Bjarne Tvedten on 20/01/2019.
+//  Copyright © 2019 Digital Mood. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
